e423db4eedbd416f6b2b207750b85b612567c7fb
Scott Chase Waggener <tidalpaladin@protonmail.com>
2022-07-12 20:52:08 -0500

    Fix version import

--- registry/__init__.py
+++ registry/__init__.py
@@ -3,9 +3,4 @@
 from .registry import RegisteredFunction, Registry, bind_relevant_kwargs, has_var_kwargs, iterate_arg_names
 
 
-try:
-    from .version import __version__
-except ImportError:
-    __version__ = "Unknown"
-
 __all__ = ["Registry", "RegisteredFunction", "iterate_arg_names", "has_var_kwargs", "bind_relevant_kwargs"]
